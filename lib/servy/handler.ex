4def969f537a604335c2511f3f67ef62d62b30c2
Shen <87602381+ShenPrime@users.noreply.github.com>
2025-08-14 21:47:16 +0300

    chapter 9

---
+++ lib/servy/handler.ex
@@ -0,0 +1,155 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    request
+    |> parse
+    |> rewrite_path
+    |> log
+    |> route
+    |> track
+    |> emojify
+    |> format_response
+  end
+
+  def emojify(%{status: 200} = conv) do
+    emojis = String.duplicate("🎉", 5)
+    body = emojis <> "\n" <> conv.resp_body <> "\n" <> emojis
+    %{conv | resp_body: body}
+  end
+
+  def emojify(conv), do: conv
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts("Warning: #{path} is on the loose!")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+    %{conv | path: "/bears/#{id}"}
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def log(conv), do: IO.inspect(conv)
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("/n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: "", status: nil}
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Yogi, Smoky, Otherone"}
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(%{method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages/", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
+  def route(%{method: "GET", path: "/bears/new"} = conv) do
+    Path.expand("../../pages/", __DIR__)
+    |> Path.join("form.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
+  def route(%{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "Operation failed: #{reason}"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+
+    """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1/n
+Host: example.com/n
+User-Agent: ExampleBrowser/1.0/n
+Accept: */*/n
+
+"""
+
+request2 = """
+GET /bears?id=2 HTTP/1.1/n
+Host: example.com/n
+User-Agent: ExampleBrowser/1.0/n
+Accept: */*/n
+
+"""
+
+request3 = """
+GET /about HTTP/1.1/n
+HOST: example.com/n
+User-Agen: ExampleBrowser/1.0/n
+Accept: */*/n
+
+"""
+
+# request4 = """
+# GET /bears/new HTTP/1.1/n
+# HOST: example.com/n
+# User-Agen: ExampleBrowser/1.0/n
+# Accept: */*/n
+#
+# """
+
+response = Servy.Handler.handle(request)
+response2 = Servy.Handler.handle(request2)
+response3 = Servy.Handler.handle(request3)
+# response4 = Servy.Handler.handle(request4)
+
+IO.puts(response)
+IO.puts(response2)
+IO.puts(response3)
+# IO.puts(response4)
