b60c9c9448f72877ced983257269e91cf691ed1c
Shen <87602381+ShenPrime@users.noreply.github.com>
2025-08-16 01:20:22 +0300

    Added: Chapter 12

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,15 @@
 defmodule Servy.Handler do
+  @moduledoc "handles HTTP request."
+
+  alias Servy.Conv
+
+  @pages_path Path.expand("pages", File.cwd!())
+
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1, emojify: 1]
+  import Servy.Parser, only: [parse: 1]
+  import Servy.Filehandler, only: [handle_file: 2]
+
+  @doc "Transforms the request into a response"
   def handle(request) do
     request
     |> parse
@@ -10,105 +21,45 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def emojify(%{status: 200} = conv) do
-    emojis = String.duplicate("🎉", 5)
-    body = emojis <> "\n" <> conv.resp_body <> "\n" <> emojis
-    %{conv | resp_body: body}
-  end
-
-  def emojify(conv), do: conv
-
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts("Warning: #{path} is on the loose!")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "/bears/#{id}"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect(conv)
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("/n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{method: method, path: path, resp_body: "", status: nil}
-  end
-
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Yogi, Smoky, Otherone"}
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages/", __DIR__)
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/bears/new"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
     Path.expand("../../pages/", __DIR__)
     |> Path.join("form.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{path: path} = conv) do
+  def route(%Conv{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File not found!"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{conv | status: 500, resp_body: "Operation failed: #{reason}"}
-  end
-
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)} 
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
 
     """
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 end
 
---
+++ lib/servy/plugins.ex
@@ -0,0 +1,31 @@
+defmodule Servy.Plugins do
+  alias Servy.Conv
+
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%Conv{path: "/bears?id=" <> id} = conv) do
+    %{conv | path: "/bears/#{id}"}
+  end
+
+  def rewrite_path(%Conv{} = conv), do: conv
+
+  def log(%Conv{} = conv), do: IO.inspect(conv)
+
+  def emojify(%Conv{status: 200} = conv) do
+    emojis = String.duplicate("🎉", 5)
+    body = emojis <> "\n" <> conv.resp_body <> "\n" <> emojis
+    %{conv | resp_body: body}
+  end
+
+  def emojify(%Conv{} = conv), do: conv
+
+  @doc "Logs 404 requests"
+  def track(%Conv{status: 404, path: path} = conv) do
+    IO.puts("Warning: #{path} is on the loose!")
+    conv
+  end
+
+  def track(%Conv{} = conv), do: conv
+end
